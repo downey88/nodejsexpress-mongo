b0efde97efdd0426e40e7138294c41dad75aaa5b
donny.tri <donny.tri88@gmail.com>
2022-07-04 13:22:57 +0700

    Change Time

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.get('/',(req,res)=>{
     res.json({
-        message:'Backend NodeJS 04 July 2022 13:19'
+        message:'Backend NodeJS 04 July 2022 13:22'
     })
 })
 
