e15326e2bcd5c7bfdd35bda07ff0563c92061ddb
donny.tri <donny.tri88@gmail.com>
2022-07-10 22:10:25 +0700

    Add Script

--- script/backend.sh
+++ script/backend.sh
@@ -1,7 +1,10 @@
 cd /home/ubuntu/backend/_work/nodejsbackend/nodejsbackend
 # npm test
-if[3>2]; then
+if[3>2] 
+then
     echo "Benar"
+    # sudo pm2 restart backend
+else
+    # sudo pm2 start app.js --name=backend
 fi
-# sudo pm2 start app.js --name=backend
-# sudo pm2 restart backend
+
